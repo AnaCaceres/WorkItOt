47a09c6cb8f1c796e4754c6169f1bf4b206392ff
Ana de Lorenzo-Cáceres Luis <anadelorenzoCL@gmail.com>
2020-12-28 20:23:57 +0100

    adding styling

---
+++ components/Head.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <h1>Work It Out!</h1>
+    <div class="buttons"></div>
+  </div>
+</template>
+
+<style scoped>
+div {
+  font-family: 'Staatliches', cursive;
+  background-color: #f0f3f4;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+h1 {
+  width: 70%;
+  margin-left: 3%;
+}
+.buttons {
+  width: 30%;
+}
+</style>
