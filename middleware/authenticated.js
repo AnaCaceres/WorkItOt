aae80a8c7892a235fbef20aeb3e18380ba7411a6
Ana de Lorenzo-Cáceres Luis <anadelorenzoCL@gmail.com>
2021-01-07 11:54:22 +0100

    CLN: deleting console.lo

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,13 +1,10 @@
 export default async function ({ store, redirect }) {
   const token = localStorage.getItem('authToken')
-  console.log(token)
   const authenticated = store.state.authenticated
   if (!authenticated && !token) {
     return redirect('/login')
   } else {
-    console.log('entro')
     const user = JSON.parse(localStorage.getItem('authUser'))
-    console.log(user)
     await store.dispatch('logUser', user)
   }
 }
